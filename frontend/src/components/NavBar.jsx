799b311136b900c856cd1966a2e3bc634252d4cd
Binit98486 <binit.98468@gmail.com>
2023-05-25 20:19:04 +0545

    change in assets folder

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-// import { icon } from "../../public/assets/icons/animalCare.svg";
+import icon from "../assets/icons/animalCare.svg";
 
 function NavBar() {
   return (
     <>
       <div className=" bg-slate-400">
         <div className="flex justify-between mx-32 ">
-          <div className="">{/* <img src={icon} alt="logo" /> */}</div>
+          <div className="w-10 h-10">
+            <img src={icon} alt="logo" height={32} />
+          </div>
           <div className="flex gap-8 ">
             <div>Features</div>
             <div>About Us</div>
