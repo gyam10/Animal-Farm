0823b8cf9e77aace71da1477987e9cf92296dd48
Gyamjo Dong <nischallama222@gmail.com>
2023-05-27 18:41:35 +0545

    footer and navbar changes

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import icon from "../assets/icons/animalCare.svg";
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
     <>
       <div className=" bg-slate-400">
-        <div className="flex justify-between mx-32 ">
+        <div className="flex justify-between mx-12 items-center p-4">
           <div className="w-10 h-10">
-            <img src={icon} alt="logo" height={32} />
+            <Link to="/">
+              <img src={icon} alt="logo" height={32} />
+            </Link>
           </div>
           <div className="flex gap-8 ">
-            <div>Features</div>
-            <div>About Us</div>
-            <div>Contact</div>
+            <div className="text-md uppercase font-semibold tracking-wider cursor-pointer hover:underline  ">
+              <Link to="/features">Features</Link>
+            </div>
+            <div className="text-md uppercase font-semibold tracking-wider cursor-pointer hover:underline ">
+              <Link to="/about">About Us</Link>
+            </div>
+            <div className="text-md uppercase font-semibold tracking-wider cursor-pointer hover:underline ">
+              <Link to="/contact">Contact</Link>
+            </div>
           </div>
         </div>
       </div>
