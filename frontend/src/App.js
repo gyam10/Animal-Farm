e761f622e782e6cf2a8dfe6a4cb90c0d509a2804
Gyamjo Dong <nischallama222@gmail.com>
2023-06-02 20:56:40 +0545

    Appjs changes

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import { Main, Features, About, Contact } from "./pages/common/index";
+import { Route, Routes } from "react-router-dom";
+import HomeLayout from "./pages/layout/HomeLayout";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomeLayout />}>
+          <Route index element={<Main />} />
+          <Route path="/features" element={<Features />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </>
+  );
+};
+
+export default App;
